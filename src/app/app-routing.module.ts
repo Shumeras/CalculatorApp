899637ceb19cf83936b12dc427d3d8375883bc2a
Shumeras <s.mackonis@gmail.com>
2020-11-24 17:14:57 +0200

    Small change to make a bit more responsive + other small changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,14 @@ import { HomeComponent } from './home/home.component';
 import { MemoryComponent } from './memory/memory.component';
 
 const routes: Routes = [
-  { path: "home", component: HomeComponent},
+  { path: "", component: HomeComponent},
   { path: "about", component: AboutComponent},
   { path: "memory", component: MemoryComponent},
-  { path: "**", redirectTo: "home", pathMatch: "full"}
+  { path: "**", redirectTo: "", pathMatch: "full"}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
